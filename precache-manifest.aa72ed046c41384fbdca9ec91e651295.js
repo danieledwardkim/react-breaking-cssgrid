922f0598720f91d2ea856cc86635ab5cf8f4cc2b
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-08-10 10:14:48 +0200

    Updates

---
+++ precache-manifest.aa72ed046c41384fbdca9ec91e651295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcae9e6c0ea946ce86da9e818b19e6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "279f1fb4ff094f405a7c",
+    "url": "./static/css/main.86f1ba7a.chunk.css"
+  },
+  {
+    "revision": "e4ac7bf4b5f3a22b9165",
+    "url": "./static/js/2.0ddb00a1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.0ddb00a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279f1fb4ff094f405a7c",
+    "url": "./static/js/main.f96561a7.chunk.js"
+  },
+  {
+    "revision": "3d0918e2c2f39231bba0",
+    "url": "./static/js/runtime-main.a4897fbc.js"
+  }
+]);
